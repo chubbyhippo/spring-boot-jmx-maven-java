19236a2e750a853a775a08b7e45aa7e1d657b8df
chubbyhippo <mkmsacc@outlook.com>
2025-09-21 14:22:56 +0700

    Add ApplicationRunner bean to keep the application running for 1 hour

--- src/main/java/io/github/chubbyhippo/demo/DemoApplication.java
+++ src/main/java/io/github/chubbyhippo/demo/DemoApplication.java
@@ -1,13 +1,22 @@
 package io.github.chubbyhippo.demo;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner keepRunning() {
+        return _ -> TimeUnit.HOURS.sleep(1);
+    }
 
 }
